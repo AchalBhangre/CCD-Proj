2ebdbbfbd0d8b5ba190f86b5bb77d309cda6521c
EC2 Default User <ec2-user@ip-172-31-26-189.eu-west-1.compute.internal>
2023-04-12 18:48:05 +0000

    scripts changes in 2 files

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,34 +1,12 @@
-#!/usr/bin/env bash
-#clean codedeploy-agent files for a fresh install
-
-sudo rm -rf /home/ec2-user/install
-
-#install CodeDeploy agent
-
-#sudo yum -y update
-
-#sudo yum -y install ruby
-
-#sudo yum -y install wget
-
-#cd /home/ec2-user
-
-#wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-
-#sudo chmod +x ./install
-
-#sudo ./install auto
-
-#update os & install python3
-
-sudo yum update -y
-
-sudo yum install -y python3 python3-pip  python3-devel
-
+# update os & install python3
+sudo yum -y update
+sudo amazon-linux-extras install -y python3
+sudo yum -y install python3-devel python3-pip
 pip3 install --user --upgrade virtualenv
-#delete app
 
+# delete app
 sudo rm -rf /home/ec2-user/CCD-Proj
+
 
 
 
